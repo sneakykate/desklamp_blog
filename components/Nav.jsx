eea0099bb302638f769d5d448a1bea2961258ae3
Jwookie55 <jwookie55@live.com>
2016-10-30 13:48:44 -0700

    added state display

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,17 +1,18 @@
 // import statements
-  import React from 'react';
-  import { Link, AsyncLink } from 'desklamp';
+import React from 'react';
+import { Link, AsyncLink } from 'desklamp';
+import { StateDisplay } from './components';
 
-const Nav = ({ powers }) => (
+const Nav = ({ state, powers }) => (
   <nav className="nav">
-    
     <ul>
-      <li > <div className="logo"><img src="http://www.desklamp.io/assets/lamp.png" width="60px" className="navlogo"/></div></li>
+      <li > <div className="logo"><img src="http://www.desklamp.io/assets/lamp.png" width="60px" className="navlogo" /></div></li>
       <li><Link view={'/home'} tag={'Home'} /></li>
       <li><Link view={'/home/nestedroute'} tag={'Nested'} /></li>
       <li><Link view={'/showstate'} tag={'ShowState'} /></li>
       <li><AsyncLink view={'/posts'} tag={'Posts'} func={powers.getPosts} /></li>
     </ul>
+    <StateDisplay state={state} powers={powers} />
   </nav>
   );
 
